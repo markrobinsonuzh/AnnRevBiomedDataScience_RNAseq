4f4f4ba70f3daae32983e38b6dd4df703beb9366
markrobinsonuzh <mark.robinson@imls.uzh.ch>
2019-02-14 22:06:14 +0100

    last edit to fig_DE

--- fig_DE.R
+++ fig_DE.R
@@ -10,7 +10,7 @@ dds <- DESeqDataSet(se, design = ~ cell + dex)
 dds <- dds[rowSums(counts(dds) >= 5) >= 4,]
 dds <- DESeq(dds, quiet=TRUE)
 
-pdf("Figure6_figDE.pdf", width = 9, height=4.5)
+pdf("Figure7_figDE.pdf", width = 9, height=4.5)
 par(mfrow=c(1,2))
 plotDispEsts(dds, ylim=c(1e-5,10))
 mtext("A", side=3, adj=-.05, padj=-.1, cex=2.5)
